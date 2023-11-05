58069600cad2a0f7d687e87900eaa1143fe5123c
Sharon Colson <sharon.colson@gmail.com>
2023-11-05 09:16:20 -0600

    lexicographic permute function so far...

--- program2.cpp
+++ program2.cpp
@@ -1,20 +1,23 @@
 /*
-     Title:     program2.cpp
-     Author:    Sharon Colson
-     Date:      November 2023
-     Purpose:   Traveling Salesman - find the lowest cost
-                tour when traveling from US to 8 other countries
-                and then back to US.
+	 Title:     program2.cpp
+	 Author:    Sharon Colson
+	 Date:      November 2023
+	 Purpose:   Traveling Salesman - find the lowest cost
+				tour when traveling from US to 8 other countries
+				and then back to US.
 */
 
+// Library dependencies
 #include <iostream>
 #include <fstream>
 #include <cmath>
+
 #include "GraphMatrix.h"
 using namespace std;
 
+// Array of constant size for country permutations
 const int SIZE = 10;
-const string COUNTRY_CODES[SIZE]={"AU", "BR", "CA", "CN", "GL", "IT", "NA", "RU", "US", "US"};
+const string COUNTRY_CODES[SIZE] = {"AU", "BR", "CA", "CN", "GL", "IT", "NA", "RU", "US", "US"};
 
 /*
 	Use the following structure to store each tour & its cost
@@ -26,40 +29,42 @@ struct Tour
 	int cost;
 };
 
-//Function Prototypes
+// Function Prototypes
 int searchCountryCode(string);
-GraphMatrix* readFileMakeMatrix();
-void printStringArray(string* arr, int size);
-void lexicographicCountryPermute(string* countries, Tour* tourOptions, int& numTours);
+GraphMatrix *readFileMakeMatrix();
+void printStringArray(string *arr, int size);
+void lexicographicCountryPermute(string *countries, Tour *tourOptions, int &numTours);
+// saveTour Function
+// findLowest function
 
 int main()
-{	
+{
 	Tour *tourOptions = new Tour[40320];
-	//read in the flight information from the file and then create the weight matrix
-	GraphMatrix * matrix = readFileMakeMatrix();
-	string * countries = new string[SIZE-2];
-	
+	// read in the flight information from the file and then create the weight matrix
+	// GraphMatrix * matrix = readFileMakeMatrix();
+	string *countries = new string[SIZE - 2];
+
 	cout << "\n\n*************************COUNTRIES*******************\n";
-	for(int x=0; x < SIZE-2; x++)
+	for (int x = 0; x < SIZE - 2; x++)
 	{
 		countries[x] = COUNTRY_CODES[x];
 		cout << countries[x] << endl;
 	}
-	
-	
-	//generate all possible tours (starting & ending with "US") using lexicographic permute algorithm
-	//you will need to call your lexicographic function, sending the modified countries array with the 8 country codes
-	int numTours =0;
+
+	// generate all possible tours (starting & ending with "US") using lexicographic permute algorithm
+	// you will need to call your lexicographic function, sending the modified countries array with the 8 country codes
+	int numTours = 0;
 	lexicographicCountryPermute(countries, tourOptions, numTours);
-	
+
 	cout << "\n\n*************************SOLUTION*******************\n";
-	//find the lowest cost tour and print it out (including the cost)
-	
-	
+	// find the lowest cost tour and print it out (including the cost)
+
 	cout << "\nHappy Traveling!\n";
-	
-	//don't forget to release anything that was dynamically allocated!
-	
+
+	// don't forget to release anything that was dynamically allocated!
+	delete[] countries;
+	delete[] tourOptions;
+
 	return 0;
 }
 /*
@@ -67,37 +72,35 @@ int main()
 	Parameters: a string with the country code like "BR"
 	Returns: an integer representing this country's index in the GraphMatrix.
 	For example, if the parameter is "BR", it should return index 1.  If the parameter is "CA", it should return index 3
-	It is returning the index value in the COUNTRY_CODES array.	
+	It is returning the index value in the COUNTRY_CODES array.
 	Implement this search function with the binary search algorithm!
 */
-
-// ********** Consider making this a recursive function, arguments will need to be adjusted. 
 int searchCountryCode(string country)
-{	
+{
 	// Set the starting search parameter
 	int left = 0;
 	// Set the ending search parameter
-	int right = SIZE - 3; // Excluding US and we only 
-	// want to search n - 1. 
+	int right = SIZE - 3; // Excluding US and we only
+	// want to search n - 1.
 	while (left <= right)
-	{	// find the mid-point
-		int middle = left + (right - left)/2;
+	{ // find the mid-point
+		int middle = left + (right - left) / 2;
 		// Check to verify if the mid-point is
 		// the country code we are looking for.
 		if (country == COUNTRY_CODES[middle])
 			return middle;
-		// else determine if we need to look in the 
-		// left or right half 
+		// else determine if we need to look in the
+		// left or right half
 		else if (country < COUNTRY_CODES[middle])
-			// If we need to search the left we reset 
+			// If we need to search the left we reset
 			// the ending search parameter
 			right = middle - 1;
-		else 
+		else
 			// If we need to search the right we
 			// reset the beginning search parameter.
 			left = middle + 1;
 	}
-	return -1; // Country code not found. 
+	return -1; // Country code not found.
 }
 
 /*
@@ -106,22 +109,22 @@ int searchCountryCode(string country)
 	adjacency matrix with the country codes.  The matrix values will be the
 	cost of the flights.
 */
-GraphMatrix* readFileMakeMatrix()
+GraphMatrix *readFileMakeMatrix()
 {
 	ifstream inFile;
-	GraphMatrix * matrix = new GraphMatrix(SIZE-1);
+	GraphMatrix *matrix = new GraphMatrix(SIZE - 1);
 	cout << "\nCreated the matrix.";
 	string country1, country2;
 	int price;
 	inFile.open("flights.txt");
 	cout << "\nReading from flights.txt\n";
-	if(inFile)
+	if (inFile)
 	{
-		while(inFile >> country1)
+		while (inFile >> country1)
 		{
 			inFile >> country2;
 			inFile >> price;
-			//add price to graph matrix
+			// add price to graph matrix
 			matrix->addEdge(searchCountryCode(country1), searchCountryCode(country2), price);
 			cout << "\nAdded edge from " << country1 << " to " << country2 << " with cost of $" << price;
 		}
@@ -140,9 +143,9 @@ GraphMatrix* readFileMakeMatrix()
 	Purpose:  this function will print the array of strings with a space
 		between each string
 */
-void printStringArray(string* arr, int size)
+void printStringArray(string *arr, int size)
 {
-	for(int x=0; x<size; x++)
+	for (int x = 0; x < size; x++)
 	{
 		cout << arr[x] << " ";
 	}
@@ -153,7 +156,76 @@ void printStringArray(string* arr, int size)
 	Title: lexicographicCountryPermute
 	Purpose: generate all country permutations using the lexicographic permutation algorithm
 */
-void lexicographicCountryPermute(string* countries, Tour* tourOptions, int& numTours)
+void lexicographicCountryPermute(string *countries, Tour *tourOptions, int &numTours)
 {
-	cout << "Do something here.";
+	// int size = SIZE - 2;
+
+	// // Create temp string to store the country codes
+	// string tempString = "";
+
+	// bool isDone = false;
+	// int lowBound = 0;
+	// int uppBound = 0;
+
+	// // Temporary variables to store during a swap
+	// char tempChar;
+	// string swapString;
+
+	cout << "\n\n\nLEXICOGRAPHIC ALGORITHM\n";
+
+	// for (int i = 0; i < size; i++)
+	// {
+	// 	tempString += countries[i];
+	// }
+	// while (!isDone)
+	// {
+	// 	for(int i = 0; i < size-1; i++)
+	// 	{
+	// 		tourOptions[numTours].tour[i] = countries[i];
+	// 	}
+	// 	tourOptions[numTours].tour[size] = "US";
+	// 	numTours++;
+
+	// 	// Find lowBound or determine if done
+	// 	isDone = true;
+	// 	for(int i = 0; i < size - 1; i++)
+	// 	{
+	// 		if (countries[i] < countries[i + 1])
+	// 		{
+	// 			isDone = false;
+	// 			lowBound = i;
+	// 		}
+	// 	}
+	// 	if (isDone)
+	// 		continue;
+		
+	// 	// Find uppBound
+	// 	for(int j = size-1; j>0; j--)
+	// 	{
+	// 		if(countries[j] > countries[lowBound])
+	// 		{
+	// 			uppBound = j;
+	// 			break;
+	// 		}
+	// 	}		
+
+	// 	// Swap
+	// 	tempChar = countries[lowBound][0];
+	// 	countries[lowBound][0] = countries[uppBound][0];
+	// 	countries[uppBound][0] = tempChar;
+
+	// 	// Reverse elements from lowBound+1 TO l-1
+	// 	swapString = "";
+	// 	for(int i = size;i > lowBound;i--)
+	// 	{
+	// 		swapString += countries[i];
+	// 	}
+	// 	for(int i = 0; i < size - lowBound; i++)
+	// 	{
+	// 		countries[lowBound+i] = swapString[i];
+	// 	}		
+	// }
+
+	cout << endl
+		 << endl;
 }
